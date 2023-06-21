63bd83ae414f3f5747202898295bc4d044b72b99
Kyle Runge <krunge1@hotmail.com>
2023-06-20 20:38:58 -0500

    Styling and modularization updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import react from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './views/Home';
+import JavaProject from './views/JavaProject';
+import PythonProject from './views/PythonProject';
+import MERNProject from './views/MERNProject';
 
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Home/>} path='/' default/>
+        <Route element={<JavaProject/>} path='/audit_app'/>
+        <Route element={<PythonProject/>} path='/python_project'/>
+        <Route element={<MERNProject/>} path='/MERN_project'/>
       </Routes>
     </BrowserRouter>
   );
