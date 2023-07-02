379150b56dee5f170a26f2f693479e63e2092d48
Kyle Runge <krunge1@hotmail.com>
2023-07-01 20:22:37 -0500

    Github link styling updates and project image

--- client/src/Controllers/MERNDetails.js
+++ client/src/Controllers/MERNDetails.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import styles from '../styles/MERNDetails.module.css'
-import MERN_Placeholder from '../assets/MERN_Placeholder.png'
+import projectImage from '../assets/portfolio_project.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const MERNDetails = () => {
     return (
@@ -13,7 +15,7 @@ const MERNDetails = () => {
                     <Link target="_blank" rel="noopener noreferrer" className={styles.link} to="https://github.com/krunge1/portfolio_website">project</Link>
                 </div>
             </div>
-            <img src={MERN_Placeholder} alt="MERN_Placeholder" className={styles.projectImage}/>
+            <img src={projectImage} alt="projectImage" className={styles.projectImage}/>
             <h2 className={styles.subHeader}>Project Overview</h2>
                 <p className={styles.projectNarrative}>This project is a multi-page personal portfolio website. It is built using React library, Javascript, HTML, and CSS. I've also mixed in a few additional npm packages such as FontAwesome and EmailJS. The website was built to showcase my skills and projects.</p>
                 <p className={styles.projectNarrative}>Feel free to check out the repository by visiting the Github Link below.</p>
@@ -25,8 +27,10 @@ const MERNDetails = () => {
                     <p className={styles.skill}>Github</p>
                     <p className={styles.skill}>FontAwesome</p>
                 </div>
-            <h2 className={styles.subHeader}>See Live</h2>
-            <Link target="_blank" rel="noopener noreferrer" className={styles.link} to="https://github.com/krunge1/portfolio_website">Github Link</Link>
+            <div className={styles.contentLive}>
+                <h2 className={styles.subHeader}>See Live</h2>
+                <Link target="_blank" rel="noopener noreferrer" className={styles.link} to="https://github.com/krunge1/portfolio_website"><FontAwesomeIcon icon={faGithub}/>Github Link</Link>
+            </div>
         </div>
     )
 }
